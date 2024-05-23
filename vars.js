9baabd87d1fe9fab90e776c83918af547dd456b1
MYHANDSLIP3D <gannonj2020@gmail.com>
2024-05-23 00:06:50 -0400

    Update vars.js

--- vars.js
+++ vars.js
@@ -121,7 +121,7 @@ const renderSettings = {
         cooldown: "#ff404080",
         heat: "#2040ff80",
 
-        meteor: loadImage("hollow-purple.jpg"),
+        meteor: loadImage("hollow-purple.png"),
         ghost: "#000000e0",
         blueFire: "#54545440",
         shield: "#383838e0",
