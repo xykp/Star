98139115de1fa9803e3ff4234c2de4fa8e37cdef
xykp <145589212+xykp@users.noreply.github.com>
2024-05-23 00:17:59 -0400

    Update vars.js

--- vars.js
+++ vars.js
@@ -323,7 +323,7 @@ const renderSettings = {
             loadImage("https://skap.io/textures/powers/shrinker.svg"),
             loadImage("https://skap.io/textures/powers/explosion.svg"),
             loadImage("https://skap.io/textures/powers/wall.svg"),
-            loadImage("hollow-purple.jpg"),
+            loadImage("hollow purple.png"),
             loadImage("https://skap.io/textures/powers/refuel.svg"),
             loadImage("https://skap.io/textures/powers/feather.svg"),
             loadImage("https://skap.io/textures/powers/shield.svg"),
