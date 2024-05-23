7ccee887e9351a1ec6ee847bcf6dca7b1b76915e
xykp <145589212+xykp@users.noreply.github.com>
2024-05-22 23:30:37 -0400

    Update vars.js

--- vars.js
+++ vars.js
@@ -123,7 +123,7 @@ const renderSettings = {
 
         meteor: "#c08000e0",
         ghost: "#20a040e0",
-        blueFire: "#3f10ff",
+        blueFire: "#40FF10",
         shield: "#383838e0",
         frost: "#00ffff40",
         tail: "#d0a020e0",
